fe912a8f6dae35ffe4640d9779f2f2eb7344fd56
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-04-13 20:50:10 +0800

    updated api routes

--- index.ts
+++ index.ts
@@ -11,58 +11,110 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.get("/todos", async (req: Request, res: Response) => {
-    const todos = await prisma.todo.findMany();
-    return res.status(200).json(todos);
-})
-
-app.post("/todo", async (req: Request, res: Response) => {
-    const { title } = req.body;
-    const newTodo = await prisma.todo.create({
-        data: {
-            title: title
+app.get("/user/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const user = await prisma.user.findMany({
+        where: {
+            id: parseInt(id)
+        },
+        include: {
+            posts: {
+                include: {
+                    comments: true
+                }
+            }
         }
     })
-    return res.status(200).json(newTodo);
+    console.log(user);
+    return res.status(200).json(user);
 });
 
-app.put("/todo/:id", async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { title } = req.body;
-    const updatedTodo = await prisma.todo.update({
-        data: {
-            title: title
+app.post("/user", async (req: Request, res: Response) => {
+    const { username, email, password } = req.body;
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password
+            }
+        })
+        return res.status(200).json(newUser);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ msg: "Error In Creating User." })
+    }
+});
+
+app.get("/posts", async (req: Request, res: Response) => {
+    const posts = await prisma.post.findMany({
+        include: {
+            comments: {
+                orderBy: {
+                    datePosted: "desc"
+                }
+            }
+        }
+    });
+    return res.status(200).json(posts);
+});
+
+app.post("/post", async (req: Request, res: Response) => {
+    const { userID, content } = req.body;
+    try {
+        const newPost = await prisma.post.create({
+            data: {
+                content: content,
+                User: {
+                    connect: {
+                        id: parseInt(userID)
+                    }
+                }
+            },
+        });
+        return res.status(200).json(newPost);
+    } catch (err) {
+        return res.status(400).json({ msg: "An Error Has Occured!" })
+    }
+});
+
+app.get("/post/:postId/comments", async (req: Request, res: Response) => {
+    const { postId } = req.params;
+    const comments = await prisma.comment.findMany({
+        where: {
+            postId: {
+                equals: parseInt(postId)
+            }
         },
-        where: {
-            id: parseInt(id)
-        }
-    });
-    return res.status(200).json(updatedTodo);
+    })
+    return res.status(200).json(comments);
 });
 
-app.put("/completetodo/:id", async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const updateTodo = await prisma.todo.update({
-        data: {
-            status: "completed"
-        },
-        where: {
-            id: parseInt(id)
-        }
-    });
-    return res.status(200).json(updateTodo);
+app.post("/comment", async (req: Request, res: Response) => {
+    const { postId, userId, content } = req.body;
+    try {
+        const newComment = await prisma.comment.create({
+            data: {
+                content,
+                Post: {
+                    connect: {
+                        id: postId
+                    }
+                },
+                User: {
+                    connect: {
+                        id: userId
+                    }
+                }
+            }
+        });
+        return res.status(200).json(newComment);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ msg: "An Error has Occured" });
+    }
 });
 
-app.delete("/todo/:id", async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const deletedTodo = await prisma.todo.delete({
-        where: {
-            id: parseInt(id)
-        }
-    });
-    return res.status(200).json(deletedTodo);
-})
-
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`);
 })
